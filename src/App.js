b8c27806a742a2b2dac32e932660f958f32f5a66
James Bubb <jpbubb82@gmail.com>
2021-05-27 19:48:50 +0100

    finished branch

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import Product from './Product';
-
+import React, { useState, useEffect } from "react";
+import Product from "./Product";
 
 export function App() {
-    const [productData, setProductData] = useState();
+  const [productData, setProductData] = useState();
 
-    useEffect(async () => {
-        const productData = await fetch('http://localhost:3009/product').then(response => response.json());
-        
-        setProductData(productData);
-    });
-    return (
-        <div className="container">
-            {productData &&
-                <Product product={productData}></Product>
-            }
-        </div>
-    )
+  useEffect(() => {
+    (async () => {
+      const productData = await fetch("http://localhost:3009/product").then(
+        (response) => response.json()
+      );
+
+      setProductData(productData);
+    })();
+  }, []);
+
+  return (
+    <div className="container">
+      {productData && <Product product={productData}></Product>}
+    </div>
+  );
 }
